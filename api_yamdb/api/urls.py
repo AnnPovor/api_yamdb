573af0fe9ea4fb7aa46fa0cbe6c34dbb23db1a70
Yuri Baranov <jerry.holidays@gmail.com>
2022-08-09 17:23:32 +0300

    categories_initial

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,31 @@
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from .views import (
+    CategoryViewSet,
+    GenreViewSet,
+    TitleViewSet
+)
+
+app_name = 'api'
+
+v1_router = SimpleRouter()
+v1_router.register(
+    'categories',
+    CategoryViewSet,
+    basename='сategories'
+)
+v1_router.register(
+    'titles',
+    TitleViewSet,
+    basename='titles'
+)
+v1_router.register(
+    'genres',
+    GenreViewSet,
+    basename='genres'
+)
+
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+]
