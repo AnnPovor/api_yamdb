c4c199fe9db8b896147f2bc7df194d016ac21265
Yuri Baranov <jerry.holidays@gmail.com>
2022-08-17 09:51:10 +0300

    22.08.17_review_1

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,8 +1,8 @@
 import uuid
 
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from django.core.validators import RegexValidator
+from django.db import models
 
 
 class User(AbstractUser):
