d89fd711f71a125edd155bbf385b54301e5a7797
Yuri Baranov <jerry.holidays@gmail.com>
2022-08-18 09:31:14 +0300

    22.08.18_review_2_validators

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,13 +3,11 @@ from rest_framework import permissions
 
 class IsAdminUserOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if (
+        return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated and (
                 request.user.is_admin or request.user.is_superuser)
-        ):
-            return True
-        return False
+        )
 
 
 class IsAdmin(permissions.BasePermission):
